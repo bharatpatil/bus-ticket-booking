97fabf244e869e9ddab202aabbb2b7427fa1bca5
Bharat Patil <sun.bharat.patil@gmail.com>
2014-11-28 12:03:38 +0530

    from and to changes to source_city and destination_city

--- database/5.data.sql
+++ database/5.data.sql
@@ -4,3 +4,22 @@ CHANGE COLUMN `departure_time` `departure_time` TIME NOT NULL ;
 update schedule set departure_time = '21:00:00' where id in (1,2);
 update schedule set departure_time = '12:00:00' where id in (3,4);
 update schedule set departure_time = '20:30:00' where id in (5);
+
+
+ALTER TABLE `route` 
+DROP FOREIGN KEY `fk_city_from`,
+DROP FOREIGN KEY `fk_city_to`;
+ALTER TABLE `route` 
+CHANGE COLUMN `from` `source_city` BIGINT(20) UNSIGNED NOT NULL ,
+CHANGE COLUMN `to` `destination_city` BIGINT(20) UNSIGNED NOT NULL ;
+ALTER TABLE `route` 
+ADD CONSTRAINT `fk_city_from`
+  FOREIGN KEY (`source_city`)
+  REFERENCES `city` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_city_to`
+  FOREIGN KEY (`destination_city`)
+  REFERENCES `city` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
--- webapp/protected/models/Route.php
+++ webapp/protected/models/Route.php
@@ -5,8 +5,8 @@
  *
  * The followings are the available columns in table 'route':
  * @property string $id
- * @property string $from
- * @property string $to
+ * @property string $source_city
+ * @property string $destination_city
  * @property integer $journey_duration
  * @property integer $distance
  * @property string $added_by
@@ -18,8 +18,8 @@
  * The followings are the available model relations:
  * @property Booking[] $bookings
  * @property Fare[] $fares
- * @property City $from0
- * @property City $to0
+ * @property City $sourceCity
+ * @property City $destinationCity
  * @property User $addedBy
  * @property User $updatedBy
  * @property Schedule[] $schedules
@@ -42,13 +42,13 @@ class Route extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('from, to, journey_duration', 'required'),
+			array('source_city, destination_city, journey_duration', 'required'),
 			array('journey_duration, distance, is_available', 'numerical', 'integerOnly'=>true),
-			array('from, to, added_by, updated_by', 'length', 'max'=>20),
+			array('source_city, destination_city, added_by, updated_by', 'length', 'max'=>20),
 			array('added_on, updated_on', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, from, to, journey_duration, distance, added_by, added_on, updated_by, updated_on, is_available', 'safe', 'on'=>'search'),
+			array('id, source_city, destination_city, journey_duration, distance, added_by, added_on, updated_by, updated_on, is_available', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -62,8 +62,8 @@ class Route extends CActiveRecord
 		return array(
 			'bookings' => array(self::HAS_MANY, 'Booking', 'route_id'),
 			'fares' => array(self::HAS_MANY, 'Fare', 'route_id'),
-			'from0' => array(self::BELONGS_TO, 'City', 'from'),
-			'to0' => array(self::BELONGS_TO, 'City', 'to'),
+			'sourceCity' => array(self::BELONGS_TO, 'City', 'source_city'),
+			'destinationCity' => array(self::BELONGS_TO, 'City', 'destination_city'),
 			'addedBy' => array(self::BELONGS_TO, 'User', 'added_by'),
 			'updatedBy' => array(self::BELONGS_TO, 'User', 'updated_by'),
 			'schedules' => array(self::HAS_MANY, 'Schedule', 'route_id'),
@@ -77,8 +77,8 @@ class Route extends CActiveRecord
 	{
 		return array(
 			'id' => 'ID',
-			'from' => 'From',
-			'to' => 'To',
+			'source_city' => 'Source City',
+			'destination_city' => 'Destination City',
 			'journey_duration' => 'Journey Duration',
 			'distance' => 'Distance',
 			'added_by' => 'Added By',
@@ -108,8 +108,8 @@ class Route extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('id',$this->id,true);
-		$criteria->compare('from',$this->from,true);
-		$criteria->compare('to',$this->to,true);
+		$criteria->compare('source_city',$this->source_city,true);
+		$criteria->compare('destination_city',$this->destination_city,true);
 		$criteria->compare('journey_duration',$this->journey_duration);
 		$criteria->compare('distance',$this->distance);
 		$criteria->compare('added_by',$this->added_by,true);
